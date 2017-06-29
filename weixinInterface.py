12ad8a930411e0867a58df0bb7c08eaf15446857
pandaxiaoyi <512467239@qq.com>
2017-06-29 23:56:57 +0800

    2017/06/29: 23:56 更新工程代码，目前支持消息类型包括：文本，图片，语音，音乐，全部同样内容返回

--- weixinInterface.py
+++ weixinInterface.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import hashlib
 import web
-import lxml
+#import lxml
 import time
 import os
 import urllib2,json
@@ -34,15 +34,56 @@ class WeixinInterface:
         #如果是来自微信的请求，则回复echostr
         if hashcode == signature:
             return echostr
+        
+    def get_token():
+        payload_access_token={
+            'grant_type':'client_credential',
+            'appid':'wxe8e8776ed21f376f',
+            'secret':'7cc3aab213bc27302cf829bcde97829d'
+        }
+        token_url='https://api.weixin.qq.com/cgi-bin/token'
+        r=requests.get(token_url,params=payload_access_token)
+        dict_result= (r.json())
+        return dict_result['access_token']
+    
+    def get_media_ID(path):
+        img_url='https://api.weixin.qq.com/cgi-bin/material/add_material'
+        payload_img={
+            'access_token':get_token(),
+            'type':'image'
+        }
+        data ={'media':open(path,'rb')}
+        r=requests.post(url=img_url,params=payload_img,files=data)
+        dict =r.json()
+        return dict['media_id']
     
     def POST(self):        
         str_xml = web.data() #获得post来的数据
         xml = etree.fromstring(str_xml)#进行XML解析
-        content=xml.find("Content").text#获得用户所输入的内容
         msgType=xml.find("MsgType").text
         fromUser=xml.find("FromUserName").text
         toUser=xml.find("ToUserName").text
         if msgType == "text":
-        	return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+ msgType)# + "数据类型是" + msgType)
+			content=xml.find("Content").text#获得用户所输入的内容
+			return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+ content)
+        elif msgType == "image":
+			picId = xml.find("MediaId").text
+			return self.render.reply_image(fromUser,toUser,int(time.time()), picId)
+        elif msgType == "voice":
+			voiceId = xml.find("MediaId").text   
+			return self.render.reply_voice(fromUser,toUser,int(time.time()), voiceId)
+        elif msgType == "video":
+			videoId = xml.find("MediaId").text
+			#return self.render.reply_vedio(fromUser,toUser,int(time.time()), videoId)
+			return self.render.reply_text(fromUser,toUser,int(time.time()), u"暂时还不支持小视频")
+        elif msgType == "location": #读取发来的位置信息
+			location_x = xml.find("Location_X").text
+			location_y = xml.find("Location_Y").text
+			location_text = xml.find("Label").text
+			return self.render.reply_text(fromUser,toUser,int(time.time()),u"经纬度为x = " + location_x + " y = " + location_y + u" 位置为：" + location_text)
+        elif msgType == "shortvideo":
+			svideoId = xml.find("MediaId").text
+            #return self.render.reply_vedio(fromUser,toUser,int(time.time()), svideoId, "title", u"描述")
+			return self.render.reply_text(fromUser,toUser,int(time.time()),svideoId)
         else:
-            return self.render.reply_text(fromUser,toUser,int(time.time()),"我现在还不能识别文本以外的数据类型")
+			return self.render.reply_text(fromUser,toUser,int(time.time()), u"类型为" + msgType +u"sorry, unsupported types")
